ac465911790c9c6f6239cd049d85341bb75a34fe
zhenggg <bianzhenggg@gmail.com>
2017-08-07 18:19:46 +0800

    优化图表

--- app/Front/Controllers/HomeController.php
+++ app/Front/Controllers/HomeController.php
@@ -4,25 +4,32 @@ namespace App\Front\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Input;
+use App\Models\Periodical;
 use App\Zhenggg\Facades\Front;
 use App\Zhenggg\Layout\Column;
 use App\Zhenggg\Layout\Content;
 use App\Zhenggg\Layout\Row;
 use App\Zhenggg\Widgets\Box;
-use App\Zhenggg\Widgets\Chart\Bar;
-use App\Zhenggg\Widgets\Chart\Doughnut;
+
 use App\Zhenggg\Widgets\Chart\Line;
-use App\Zhenggg\Widgets\Chart\Pie;
-use App\Zhenggg\Widgets\Chart\PolarArea;
-use App\Zhenggg\Widgets\Chart\Radar;
-use App\Zhenggg\Widgets\Collapse;
+
 use App\Zhenggg\Widgets\InfoBox;
-use App\Zhenggg\Widgets\Tab;
-use App\Zhenggg\Widgets\Table;
+
 use Carbon\Carbon;
 
 class HomeController extends Controller
 {
+    public $dt;
+    public $month_start;
+    public $month_end;
+
+    public function __construct()
+    {
+        $this->dt = Carbon::now();
+        $this->month_start = $this->dt->startOfMonth()->toDateTimeString();
+        $this->month_end = $this->dt->endOfMonth()->toDateTimeString();
+    }
+
     public function index()
     {
         return Front::content(function (Content $content) {
@@ -31,143 +38,92 @@ class HomeController extends Controller
             $content->description('发行管理系统');
 
             $content->row(function ($row) {
+                $row->column(1, '');
                 $day_start = Carbon::today()->toDateTimeString();
                 $day_end = Carbon::tomorrow()->toDateTimeString();
-                $day_datetime = ['created_at'=>['start'=>$day_start,'end'=>$day_end]];
-                $day_order_url = '/front/finance/input?'. http_build_query($day_datetime);
+                $day_datetime = ['created_at' => ['start' => $day_start, 'end' => $day_end]];
+                $day_order_url = '/front/finance/input?' . http_build_query($day_datetime);
 
                 $day_input_num = Input::where('user_id', Front::user()->user_id)
-                    ->whereBetween('created_at', [$day_start,$day_end])
-                    ->count();
-                $row->column(3, new InfoBox('今日订单', 'shopping-cart', 'aqua', $day_order_url, $day_input_num));
+                    ->whereBetween('created_at', [$day_start, $day_end])
+                    ->sum('num');
+                $row->column(3, new InfoBox('今日订单份数', 'shopping-cart', 'aqua', $day_order_url, $day_input_num));
+                $row->column(2, '');
 
-                $month_start = Carbon::now()->startOfMonth()->toDateTimeString();
-                $month_end = Carbon::now()->endOfMonth()->toDateTimeString();
-                $month_datetime = ['created_at'=>['start'=>$month_start,'end'=>$month_end]];
-                $month_order_url = '/front/finance/input?'. http_build_query($month_datetime);
+
+                $month_datetime = ['created_at' => ['start' => $this->month_start, 'end' => $this->month_end]];
+                $month_order_url = '/front/finance/input?' . http_build_query($month_datetime);
 
                 $month_input_num = Input::where('user_id', Front::user()->user_id)
-                    ->whereBetween('created_at', [$month_start,$month_end])
-                    ->count();
-                $row->column(3, new InfoBox('今月订单', 'shopping-cart', 'green', $month_order_url, $month_input_num));
+                    ->whereBetween('created_at', [$this->month_start, $this->month_end])
+                    ->sum('num');
+                $row->column(3, new InfoBox('今月订单份数', 'shopping-cart', 'green', $month_order_url, $month_input_num));
 
 
-
-//                $data = $this->getSellerQuData('2017-08-01 00:00:00','2017-08-31 00:00:00');
-//                dd($data);
             });
+            $p_ids = Periodical::all()->pluck('name', 'id');
+            foreach ($p_ids as $p_id => $p_name) {
+                $content->row(function (Row $row) use ($p_id, $p_name) {
+                    $row->column(1, '');
+                    $row->column(10, function (Column $column) use ($p_id, $p_name) {
 
-            $content->row(function (Row $row) {
+                        $data = $this->getSellerQuData($p_id, $this->month_start, $this->month_end);
 
-//                $row->column(6, function (Column $column) {
-//
-//                    $tab = new Tab();
-//
-//                    $pie = new Pie([
-//                        ['Stracke Ltd', 450], ['Halvorson PLC', 650], ['Dicki-Braun', 250], ['Russel-Blanda', 300],
-//                        ['Emmerich-O\'Keefe', 400], ['Bauch Inc', 200], ['Leannon and Sons', 250], ['Gibson LLC', 250],
-//                    ]);
-//
-//                    $tab->add('Pie', $pie);
-//                    $tab->add('Table', new Table());
-//                    $tab->add('Text', 'blablablabla....');
-//
-//                    $tab->dropDown([['Orders', '/admin/orders'], ['administrators', '/admin/administrators']]);
-//                    $tab->title('Tabs');
-//
-//                    $column->append($tab);
-//
-//                    $collapse = new Collapse();
-//
-//                    $bar = new Bar(
-//                        ["January", "February", "March", "April", "May", "June", "July"],
-//                        [
-//                            ['First', [40,56,67,23,10,45,78]],
-//                            ['Second', [93,23,12,23,75,21,88]],
-//                            ['Third', [33,82,34,56,87,12,56]],
-//                            ['Forth', [34,25,67,12,48,91,16]],
-//                        ]
-//                    );
-//                    $collapse->add('Bar', $bar);
-//                    $collapse->add('Orders', new Table());
-//                    $column->append($collapse);
-//
-//                    $doughnut = new Doughnut([
-//                        ['Chrome', 700],
-//                        ['IE', 500],
-//                        ['FireFox', 400],
-//                        ['Safari', 600],
-//                        ['Opera', 300],
-//                        ['Navigator', 100],
-//                    ]);
-//                    $column->append((new Box('Doughnut', $doughnut))->removable()->collapsable()->style('info'));
-//                });
+                        $month_day_num = $this->dt->diffInDays($this->dt->copy()->addMonth());
 
-                $row->column(6, function (Column $column) {
+                        for ($i = 1; $i <= $month_day_num; $i++) {
+                            $month_day_array[] = sprintf("%02d", $i);
+                        }
+                        foreach ($data as $key => $value) {
+                            $dates_array[$value->dates] = $value->money;
+                        }
+                        foreach ($month_day_array as $dayk => $day) {
+                            $bool = array_key_exists($day, $dates_array);
+                            if ($bool) {
+                                $month_money_array[$dayk] = $dates_array[$day];
+                            } else {
+                                $month_money_array[$dayk] = 0;
+                            }
+                        }
 
-//                    $column->append(new Box('Radar', new Radar()));
-//
-//                    $polarArea = new PolarArea([
-//                        ['Red', 300],
-//                        ['Blue', 450],
-//                        ['Green', 700],
-//                        ['Yellow', 280],
-//                        ['Black', 425],
-//                        ['Gray', 1000],
-//                    ]);
-//                    $column->append((new Box('Polar Area', $polarArea))->removable()->collapsable());
-//
-//                    $column->append((new Box('Line', new Line()))->removable()->collapsable()->style('danger'));
+                        $line = new Line($month_day_array,
+                            [
+                                ['', $month_money_array, 'rgba(75,192,192,1)'],
+                            ]
+                        );
+                        $column->append((new Box($p_name . '——' . $this->dt->month . '月订单统计(份)', $line)));
+                    });
                 });
-
-            });
-//
-//            $headers = ['Id', 'Email', 'Name', 'Company', 'Last Login', 'Status'];
-//            $rows = [
-//                [1, 'labore21@yahoo.com', 'Ms. Clotilde Gibson', 'Goodwin-Watsica', '1997-08-13 13:59:21', 'open'],
-//                [2, 'omnis.in@hotmail.com', 'Allie Kuhic', 'Murphy, Koepp and Morar', '1988-07-19 03:19:08', 'blocked'],
-//                [3, 'quia65@hotmail.com', 'Prof. Drew Heller', 'Kihn LLC', '1978-06-19 11:12:57', 'blocked'],
-//                [4, 'xet@yahoo.com', 'William Koss', 'Becker-Raynor', '1988-09-07 23:57:45', 'open'],
-//                [5, 'ipsa.aut@gmail.com', 'Ms. Antonietta Kozey Jr.', 'Braun Ltd', '2013-10-16 10:00:01', 'open'],
-//            ];
-//
-//            $content->row((new Box('Table', new Table($headers, $rows)))->style('info')->solid());
+            }
         });
     }
 
     /**
      * [getSellerQuData 获取商户结算数据 曲线]
+     * @param  [mix] $p_id p_id
      * @param  [string] $start [起始时间]2017-08
      * @param  [string] $end   [结束时间]
      * @return [type]        [description]
      */
-    private function getSellerQuData($start,$end){
-
-        //计算时间差值,以决定格式化时间格式
-        $diff = strtotime($end)-strtotime($start);
-
-        //分组条件 1天内按小时分组,否则按天/月分组
-        //86400/1天 2678400/1月
-        if($diff<86400&&$diff>0){
-            $sort = '%H';
-        }elseif($diff<2678400){
-            $sort = '%Y-%m-%d';
-        }else{
-            $sort = '%Y-%m';
-        }
+    private function getSellerQuData($p_id, $start, $end)
+    {
         //把数据添加时间按格式化时间分组求和,求和分两种,一种是直接求和,一种是满足case when条件的数据求和
-        $query = \DB::table('inputs as i')->select(\DB::raw("FROM_UNIXTIME(created_at,'{$sort}') as thedata,sum(case when i.input_status = 1 then i.p_amount end) as xiabi,sum(case when i.input_status = 0 then i.p_amount end) as online,sum(i.p_amount) as alls"))->groupBy(\DB::raw("FROM_UNIXTIME(created_at,'{$sort}')"));
+        $query = \DB::table('inputs as i')
+            ->select(\DB::raw("FROM_UNIXTIME(unix_timestamp(created_at),'%d') as dates,sum(case when i.input_status = 1 then i.p_amount end) as money"))
+            ->groupBy(\DB::raw("dates"));
 
+        if (!empty($p_id)) {
+            $query->whereRaw('i.p_id = ?', $p_id);
+        }
         //条件筛选 某时间段内
-        if( !empty($start) ){
-            $query->whereRaw('i.created_at >= ?',strtotime($start));
+        if (!empty($start)) {
+            $query->whereRaw('i.created_at >= ?', $start);
         }
-        if( !empty($end) ){
-            $query->whereRaw('i.created_at <= ?',strtotime($end));
+        if (!empty($end)) {
+            $query->whereRaw('i.created_at <= ?', $end);
         }
 
-        $data = $query->get();
+        return $query->get()->toArray();
 
-        return $data;
     }
 }
--- app/Zhenggg/Widgets/Chart/Line.php
+++ app/Zhenggg/Widgets/Chart/Line.php
@@ -2,6 +2,8 @@
 
 namespace App\Zhenggg\Widgets\Chart;
 
+use Illuminate\Support\Arr;
+
 class Line extends Chart
 {
     protected $colorNames = [
@@ -23,52 +25,58 @@ class Line extends Chart
         'yellow'  => [255, 255, 0],
     ];
 
+    protected $options = [
+        'responsive' => true,
+        'maintainAspectRatio' => false,
+    ];
+
+    protected $labels = [];
+
+    public function __construct($labels = [], $data = [])
+    {
+        $this->data['labels'] = $labels;
+
+        $this->data['datasets'] = [];
+
+        $this->add($data);
+    }
+
+    public function add($label, $data = [], $fillColor = 'rgba(75,192,192,1)')
+    {
+        if (is_array($label)) {
+            if (Arr::isAssoc($label)) {
+                $this->data[] = $label;
+            } else {
+                foreach ($label as $item) {
+                    call_user_func_array([$this, 'add'], $item);
+                }
+            }
+
+            return $this;
+        }
+
+        $this->data['datasets'][] = [
+            'label'         => $label,
+            'fill'         => false,
+            'fillColor'     => $fillColor,
+            'data'          => $data,
+
+        ];
+
+        return $this;
+    }
+
     public function script()
     {
+        $data = json_encode($this->data);
         $options = json_encode($this->options);
-
         return <<<EOT
 
-(function(){
-    var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(0,255,0,0.2)",
-                //strokeColor: "rgba(220,220,220,1)",
-                //pointColor: "rgba(220,220,220,1)",
-                //pointStrokeColor: "#fff",
-                //pointHighlightFill: "#fff",
-                //pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                //strokeColor: "rgba(151,187,205,1)",
-                //pointColor: "rgba(151,187,205,1)",
-                //pointStrokeColor: "#fff",
-                //pointHighlightFill: "#fff",
-                //pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            },
-            {
-                label: "My Third dataset",
-                fillColor: "rgba(255,255,0,0.2)",
-                //strokeColor: "rgba(151,187,205,1)",
-                //pointColor: "rgba(151,187,205,1)",
-                //pointStrokeColor: "#fff",
-                //pointHighlightFill: "#fff",
-                //pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [56, 78, 12, 34, 35, 67, 90]
-            }
-        ]
-    };
 
     var canvas = $("#{$this->elementId}").get(0).getContext("2d");
-    var chart = new Chart(canvas).Line(data, $options);
-})();
+    var chart = new Chart(canvas).Line($data, $options);
+
+
 EOT;
     }
 }
